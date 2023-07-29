8148505d457964b57909c6445efd5821ca503b8f
码不动了 <79295797@qq.com>
2023-07-29 20:25:59 +0800

    add

---
+++ assets/index.html-2f83b6da.js
@@ -0,0 +1,6 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as a,c as n,d as s}from"./app-6ec465d3.js";const i={},d=s(`<blockquote><p>博客搭建好的第一个测试文章，只用于测试后期删除</p></blockquote><h2 id="第一个代码" tabindex="-1"><a class="header-anchor" href="#第一个代码" aria-hidden="true">#</a> 第一个代码</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code>@echo off
+
+net start mysql
+
+<span class="token builtin class-name">exit</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="第二次尝试" tabindex="-1"><a class="header-anchor" href="#第二次尝试" aria-hidden="true">#</a> 第二次尝试</h2><p>我的尝试如下</p><blockquote><p>12367830831418670</p></blockquote>`,6),c=[d];function t(l,r){return a(),n("div",null,c)}const u=e(i,[["render",t],["__file","index.html.vue"]]);export{u as default};
